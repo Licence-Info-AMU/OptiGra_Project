f80ea996da168dd85a7c7ea438c8be735f0902fd
moonlight83340 <moonlight83340@users.noreply.github.com>
2017-03-17 04:53:54 +0100

    Update menus.c

--- menus.c
+++ menus.c
@@ -10,54 +10,12 @@
 #include "menus.h"
 
 // Callbacks menus / boutons
-void on_item_load_activate (GtkWidget *widget, gpointer data){
-    Mydata *my = get_mydata(data);
-    GtkWidget *dialog;
-    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
-    gint res;
-
-    dialog = gtk_file_chooser_dialog_new ("Load Image",
-                                          GTK_WINDOW(my->window),
-                                          action,
-                                          "Cancel",
-                                          GTK_RESPONSE_CANCEL,
-                                          "Open",
-                                          GTK_RESPONSE_ACCEPT,
-                                          NULL);
-    if (my->current_folder != NULL) 
-        gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER (dialog), my->current_folder);
-    res = gtk_dialog_run (GTK_DIALOG (dialog));
-    if (res == GTK_RESPONSE_ACCEPT)    {
-        char *filename;
-        GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
-        filename = gtk_file_chooser_get_filename (chooser);
-        set_status(my->status, "Loading image ...");
-        g_clear_object(&my->pixbuf);
-        my->pixbuf = gdk_pixbuf_new_from_file(filename, NULL);       
-        if (my->pixbuf == NULL) { 
-            set_status(my->status, "Loading failed : not an image.");
-        }
-        else {
-            set_status(my->status, "Loading success : image %dx%d.",gdk_pixbuf_get_width(my->pixbuf),gdk_pixbuf_get_height(my->pixbuf));
-            my->scale_horizon_value = 1.0;
-            my->rotate_angle = 0.0;
-            apply_image_transforms (my);
-        }
-        my->current_folder = NULL;
-        my->current_folder = gtk_file_chooser_get_current_folder(chooser);    
-        g_free(filename);    
-    }
-
-    gtk_widget_destroy (dialog);
-    
-    printf ("Button clicked in window ’%s’\n", my->title);
-}
 
 void on_item_about_activate (GtkWidget *widget, gpointer data){
     Mydata *my = get_mydata(data);
     char *auteurs[] = {"Florian Duarte <florian.duarte@etu.univ-amu.fr>","Gaetan Perrot <gaetan.perrot@etu.univ-amu.fr>", NULL};
     gtk_show_about_dialog (NULL, "program-name", my->title, "version", 
-                           "0.1", "website", "http://j.mp/optigra", 
+                           "0.1", "website", "https://github.com/moonlight83340/OptiGra_Project", 
                            "authors", auteurs, "logo-icon-name", 
                            "face-wink",NULL);    
     printf ("About\n");
@@ -66,57 +24,6 @@ void on_item_about_activate (GtkWidget *widget, gpointer data){
 void on_item_quit_activate (GtkWidget *widget, gpointer data){
     Mydata *my = get_mydata(data);
     gtk_widget_destroy(my->window);
-}
-
-void on_item_color_activate (GtkWidget *widget, gpointer data){
-    Mydata *my = get_mydata(data);
-    gint res = 0;
-    printf ("Color\n");
-    GtkWidget *color_chooser;
-    color_chooser = gtk_color_chooser_dialog_new ("Background color", NULL);
-    res = gtk_dialog_run(GTK_DIALOG (color_chooser));
-    if (res == GTK_RESPONSE_OK) { 
-        GdkRGBA bg_color;
-        gtk_color_chooser_get_rgba (GTK_COLOR_CHOOSER (color_chooser), &bg_color);
-        gchar *c;
-        c = gdk_rgba_to_string (&bg_color);
-        set_status (my->status, "Selected bg color : %s", c);
-        gtk_widget_override_background_color(my->area, GTK_STATE_FLAG_NORMAL, &bg_color);
-        g_free(c);
-    }
-    gtk_widget_destroy(color_chooser);
-}
-
-void on_item_rotate_activate (GtkWidget *widget, gpointer data){
-    Mydata *my = get_mydata(data);
-    if (my->pixbuf != NULL){
-        my->rotate_angle = my->rotate_angle + 90;
-        if (my->rotate_angle >= 360) 
-            my->rotate_angle = my->rotate_angle - 360;
-        update_area_with_transforms (my);
-        
-        set_status(my->status, "Image rotated.");
-    }
-    else 
-        set_status(my->status, "No image to rotate.");
-}
-
-void on_item_scale_activate (GtkWidget *widget, gpointer data){
-    Mydata *my = get_mydata(data);     
-    gtk_range_set_value(GTK_RANGE(my->scale_horizon),my->scale_horizon_value);
-    gtk_window_present (GTK_WINDOW (my->win_scale));
-}
-
-void on_item_clip_activate (GtkCheckMenuItem *widget, gpointer data){
-    Mydata *my = get_mydata(data);    
-    
-    my->clip_image = gtk_check_menu_item_get_active (widget);
-    if (my->clip_image == TRUE)
-        set_status(my->status, "Clipping is on");
-    else
-        set_status(my->status, "Clipping is off");
-        
-    refresh_area (my->area);
 }
 
 void on_item_edit_activate (GtkCheckMenuItem *widget, gpointer data){
@@ -137,72 +44,54 @@ void on_item_edit_activate (GtkCheckMenuItem *widget, gpointer data){
 
 void menu_init (gpointer user_data){
 	Mydata *my = get_mydata(user_data);  
-    GtkWidget *item_file, *item_tools, 
-              *item_help, *sub_file, *item_load, *item_quit, *sub_tools,
-              *item_rotate, *item_color, *item_scale, *sub_help, *item_about,
-              *item_clip, *item_edit; 
+    GtkWidget *item_game, *item_level, *item_help, 
+				*sub_game, *item_start, *item_pause, *item_new_game, *item_quit, 
+				*sub_level, *sub_help, *item_about,*item_edit,*item_new_level,*item_save; 
     my->menu_bar = gtk_menu_bar_new();    
     //Menu    
-    item_file = gtk_menu_item_new_with_label ("File");
-    item_tools = gtk_menu_item_new_with_label ("Tools");
+    item_game = gtk_menu_item_new_with_label ("Game");
+    item_level = gtk_menu_item_new_with_label ("Level");
     item_help = gtk_menu_item_new_with_label ("Help");
     
-    gtk_menu_shell_append(GTK_MENU_SHELL(my->menu_bar), item_file);
-    gtk_menu_shell_append(GTK_MENU_SHELL(my->menu_bar), item_tools);
+    gtk_menu_shell_append(GTK_MENU_SHELL(my->menu_bar), item_game);
+    gtk_menu_shell_append(GTK_MENU_SHELL(my->menu_bar), item_level);
     gtk_menu_shell_append(GTK_MENU_SHELL(my->menu_bar), item_help);
     
-    //File
-    sub_file = gtk_menu_new ();
+    //Game
+    sub_game = gtk_menu_new ();
     
-    item_load = gtk_menu_item_new_with_label ("Load");
+    item_new_game = gtk_menu_item_new_with_label ("New");
+    item_start = gtk_menu_item_new_with_label ("Start");
+    item_pause = gtk_menu_item_new_with_label ("Pause");
     item_quit = gtk_menu_item_new_with_label ("Quit");
+           
+    gtk_menu_shell_append(GTK_MENU_SHELL(sub_game), item_new_game);                     
+	gtk_menu_shell_append(GTK_MENU_SHELL(sub_game), item_start);
+    gtk_menu_shell_append(GTK_MENU_SHELL(sub_game), item_pause);
+    gtk_menu_shell_append(GTK_MENU_SHELL(sub_game), item_quit);
     
-    g_signal_connect (item_load, "activate",
-                      G_CALLBACK(on_item_load_activate), my);
-    g_signal_connect (item_quit, "activate",
-                      G_CALLBACK(on_item_quit_activate), my);
-                          
-    gtk_menu_shell_append(GTK_MENU_SHELL(sub_file), item_load);
-    gtk_menu_shell_append(GTK_MENU_SHELL(sub_file), item_quit);
+    //Level
+    sub_level = gtk_menu_new ();
     
-    //Tools
-    sub_tools = gtk_menu_new ();
-    
-    item_rotate = gtk_menu_item_new_with_label ("Rotate");
-    item_color = gtk_menu_item_new_with_label ("Bg Color");
-    item_scale = gtk_menu_item_new_with_label ("Scale");
-    item_clip = gtk_check_menu_item_new_with_label ("Clip");
-    item_edit = gtk_check_menu_item_new_with_label ("Editing");
+    item_edit = gtk_check_menu_item_new_with_label ("Edit");
+    item_new_level = gtk_check_menu_item_new_with_label ("New");
+    item_save = gtk_check_menu_item_new_with_label ("Save");
 
-    g_signal_connect (item_rotate, "activate",
-                      G_CALLBACK(on_item_rotate_activate), my);
-    g_signal_connect (item_color, "activate",
-                      G_CALLBACK(on_item_color_activate), my);
-    g_signal_connect (item_scale, "activate",
-                      G_CALLBACK(on_item_scale_activate), my);                  
-    g_signal_connect (item_clip, "activate",
-                      G_CALLBACK(on_item_clip_activate), my);              
-    g_signal_connect (item_edit, "activate",
-                      G_CALLBACK(on_item_edit_activate), my);                      
+    g_signal_connect (item_edit, "activate",G_CALLBACK(on_item_edit_activate), my);                   
                                               
-    gtk_menu_shell_append(GTK_MENU_SHELL(sub_tools), item_rotate);
-    gtk_menu_shell_append(GTK_MENU_SHELL(sub_tools), item_color);
-    gtk_menu_shell_append(GTK_MENU_SHELL(sub_tools), item_scale);
-    gtk_menu_shell_append(GTK_MENU_SHELL(sub_tools), item_clip);
-    gtk_menu_shell_append(GTK_MENU_SHELL(sub_tools), item_edit);
+    gtk_menu_shell_append(GTK_MENU_SHELL(sub_level), item_edit);
+    gtk_menu_shell_append(GTK_MENU_SHELL(sub_level), item_new_level);
+    gtk_menu_shell_append(GTK_MENU_SHELL(sub_level), item_save);
     
     //Help
     sub_help = gtk_menu_new ();
     
     item_about = gtk_menu_item_new_with_label ("About");
-    
-    g_signal_connect (item_about, "activate",
-                      G_CALLBACK(on_item_about_activate), my);    
-                      
+    g_signal_connect (item_about, "activate",G_CALLBACK(on_item_about_activate), my);  
     gtk_menu_shell_append(GTK_MENU_SHELL(sub_help), item_about);
     
     //Sub-menus    
-    gtk_menu_item_set_submenu (GTK_MENU_ITEM(item_file), sub_file);
-    gtk_menu_item_set_submenu (GTK_MENU_ITEM(item_tools), sub_tools);
+    gtk_menu_item_set_submenu (GTK_MENU_ITEM(item_game), sub_game);
+    gtk_menu_item_set_submenu (GTK_MENU_ITEM(item_level), sub_level);
     gtk_menu_item_set_submenu (GTK_MENU_ITEM(item_help), sub_help);    
 }
